1782df8132a9e94821bac05a7225edfed4bb0068
Devin Bedford <bedforddevin895@gmail.com>
2023-08-08 17:44:43 +0200

    Fix intro button loading before intended.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const introBtn = document.querySelector(".intro button");
 const footerText = document.querySelector("footer p");
 const date = new Date();
-introBtn.style.opacity = 0;
 console.log(introBtn);
 setTimeout(addClass, 2000);
 function addClass(){
